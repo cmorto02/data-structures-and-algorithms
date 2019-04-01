df117de4d4412a0dbbfada2c2222958a1ecd65f7
Chris <chrismorton.cm@gmail.com>
2019-04-01 11:53:28 -0700

    working on queues

---
+++ code-challenges/data structures/stacksAndQueues/stacksAndQueues/stacksAndQueues/Classes/Queue.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace stacksAndQueues.classes
+{
+    class Queue<T>
+    {
+        public Node<T> Front { get; set; }
+        public Node<T> Current { get; set; }
+        public Node<T> Rear { get; set; }
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>
+            {
+                Value = data,
+                Next = Front
+            };
+            Rear = node;
+        }
+        public Node<T> Dequeue()
+        {
+            
+        }
+        public T Peek()
+        {
+            return Front.Value;
+        }
+    }
+}
