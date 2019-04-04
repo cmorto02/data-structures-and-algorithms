43e1172c5174345a44a019c12aa049e19a4e166c
Chris <chrismorton.cm@gmail.com>
2019-04-04 11:54:06 -0700

    method complete

---
+++ code-challenges/challenges/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,38 @@
+﻿using stacksAndQueues;
+using stacksAndQueues.classes;
+using System;
+using System.Text;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "({[{()t}t]t}t])";
+            string result = Convert.ToString(MultiBracketValidation(input));
+            Console.WriteLine(result);
+        }
+        public static bool MultiBracketValidation(string input)
+        {
+            Stack<char> main = new Stack<char>();
+            char[] charInputArray = input.ToCharArray();
+            for (int i = 0; i < charInputArray.Length; i++)
+            {
+                if (charInputArray[i] == '(' || charInputArray[i] == '{' || charInputArray[i] == '[')
+                {
+                    main.Push(charInputArray[i]);
+                }
+                if (charInputArray[i] == ')' || charInputArray[i] == '}' || charInputArray[i] == ']')
+                {
+                    char result = main.Pop().Value;
+                    if (charInputArray[i] == ')' && result != '(' || charInputArray[i] == '}' && result != '{' || charInputArray[i] == ']' && result != '[')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
