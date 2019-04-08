8d2ca713dbcaeeb7bb857594f3203f0024efd0b6
Chris <chrismorton.cm@gmail.com>
2019-04-07 22:43:22 -0700

    done

--- code-challenges/data structures/stacksAndQueues/stacksAndQueues/stacksAndQueues/Classes/Queue.cs
+++ code-challenges/data structures/stacksAndQueues/stacksAndQueues/stacksAndQueues/Classes/Queue.cs
@@ -9,7 +9,10 @@ namespace stacksAndQueues.classes
         public Node<T> Front { get; set; }
         public Node<T> Current { get; set; }
         public Node<T> Rear { get; set; }
-
+        /// <summary>
+        /// Method adds nodes to the end of the queue
+        /// </summary>
+        /// <param name="data">this is the data that goes into the node</param>
         public void Enqueue(T data)
         {
             
@@ -28,7 +31,10 @@ namespace stacksAndQueues.classes
             Rear.Next = node;
             Rear = node;
         }
-
+        /// <summary>
+        /// This method removes a node from the front of the queue
+        /// </summary>
+        /// <returns>Returns the dequeued node</returns>
         public Node<T> Dequeue()
         {
             Node<T> temp = Front;
@@ -36,7 +42,10 @@ namespace stacksAndQueues.classes
             temp.Next = null;
             return temp;
         }
-
+        /// <summary>
+        /// This method checks the value of the front node of the queue
+        /// </summary>
+        /// <returns>Returns the value of the front node.</returns>
         public T Peek()
         {
             return Front.Value;
