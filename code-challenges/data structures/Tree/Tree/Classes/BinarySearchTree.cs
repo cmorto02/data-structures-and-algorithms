53dffae35de8cc2b338f6828f8dacc91524a69c2
Chris <chrismorton.cm@gmail.com>
2019-04-08 11:09:26 -0700

    still working on this one

---
+++ code-challenges/data structures/Tree/Tree/Classes/BinarySearchTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Tree.Classes;
+
+namespace Tree.Classes
+{
+    public class BinarySearchTree<T>
+    {
+        public Node<int> Root {get; set;}
+
+        public void Add(int value)
+        {
+            Node<int> node = new Node<int>
+            {
+                Value = value
+            };
+            if (Root == null)
+            {
+                node = Root;
+            }
+            if (node.Value < Root.Value)
+            {
+                node = Root.LeftChild;
+            }
+            if (node.Value > Root.Value)
+            {
+                node = Root.RightChild;
+            }
+        }
+    }
+}
---
+++ code-challenges/data structures/Tree/Tree/Classes/BinaryTree.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree.Classes
+{
+    class BinaryTree<T>
+    {
+        public Node<T> Root { get; set; }
+        public Node<T> Current { get; set; }
+        Queue<T> queue = new Queue<T>();
+       
+        public void PreOrder(Node<T> Root, Queue<T> queue)
+        {
+            queue.Enqueue(Root.Value);
+            if (Root.LeftChild != null)
+            {
+                PreOrder(Root.LeftChild, queue);
+            }
+            if (Root.RightChild != null)
+            {
+                PreOrder(Root.RightChild, queue);
+            }
+        }
+        public void InOrder(Node<T> Root, Queue<T> queue)
+        {
+            if (Root.LeftChild != null)
+            {
+                InOrder(Root.LeftChild, queue);
+            }
+            queue.Enqueue(Root.Value);
+            if (Root.RightChild != null)
+            {
+                InOrder(Root.RightChild, queue);
+            }
+        }
+        public void PostOrder(Node<T> Root, Queue<T> queue)
+        {
+            if (Root.LeftChild != null)
+            {
+                PostOrder(Root.LeftChild, queue);
+            }
+            if (Root.RightChild != null)
+            {
+                PostOrder(Root.RightChild, queue);
+            }
+            queue.Enqueue(Root.Value);
+
+        }
+    }
+}
