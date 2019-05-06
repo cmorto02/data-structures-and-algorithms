b72d74190c3e25c548e3df5148ef0d585ff83d03
Chris <chrismorton.cm@gmail.com>
2019-05-06 11:33:00 -0700

    still working on the last portion of this

---
+++ code-challenges/data structures/graph/graph/Program.cs
@@ -0,0 +1,31 @@
+﻿using graph.classes;
+using System;
+
+namespace graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Graph<int> graph = new Graph<int>();
+
+            var a = graph.AddVertex(7);
+            var b = graph.AddVertex(8);
+            var c = graph.AddVertex(9);
+
+            graph.AddUndirectedEdge(a, b, 4);
+            graph.AddUndirectedEdge(b, c, 5);
+            graph.AddUndirectedEdge(a, c, 6);
+
+            graph.Count();
+
+            var vertexList = graph.GetVertices();
+
+            foreach (var item in vertexList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
