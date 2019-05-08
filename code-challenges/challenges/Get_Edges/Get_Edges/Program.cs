f87d8e3f960b9d7725b4459fbe62971d09c644fe
Chris <chrismorton.cm@gmail.com>
2019-05-08 11:42:33 -0700

    added image

---
+++ code-challenges/challenges/Get_Edges/Get_Edges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Get_Edges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ code-challenges/challenges/try3/try3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace try3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public LinkedListNode<int> Partition(LinkedListNode<int> head, int x)
+        {
+            LinkedListNode<int> partition;
+            LinkedListNode<int> current = head;
+            while (current.Value != x)
+            {
+                current = current.Next;
+            }
+            partition = current;
+            LinkedListNode<int> testCurrent = head;
+            while (testCurrent != null)
+            {
+                if (testCurrent.Value > partition.Value)
+                {
+                    head = testCurrent.Next;
+                    
+                }
+            }
+            return head;
+        }
+    }
+}
